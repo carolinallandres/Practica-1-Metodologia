e6b56a3903f3f1aee72c4ad01a5df038c94e8bc1
Isa <isabelgp.ins@gmail.com>
2026-03-18 23:04:22 +0100

    Update Book.java

--- src/es/uah/matcomp/mp/e1/ejerciciosclases/practica_b/a/Book.java
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/practica_b/a/Book.java
@@ -1,42 +1,71 @@
 package es.uah.matcomp.mp.e1.ejerciciosclases.practica_b.a;
 
 public class Book {
+    private String isbn;
     private String name;
-    private Author author;
+    private Author author;    //Agregación del objeto Author
     private double price;
-    private int qty = 0;
+    private int qty;
 
-    public Book(String name, Author author, double price) {
-        this.name = name;
-        this.author = author;
+    //CONSTRUCTORES
+
+    public Book (String isbn, String name, Author author, double price){
+        this.isbn=isbn;
+        this.name=name;
+        this.author=author;
+        this.price=price;
+        qty=0;
+    }
+
+    public Book (String isbn, String name, Author author, double price,int qty){
+        this.isbn=isbn;
+        this.name=name;
+        this.author=author;
+        this.price=price;
+        this.qty=qty;
+    }
+
+    //GETTERS
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    //Getters del objeto Author
+
+    public String getAuthorName(){
+        return author.getName();
+    }
+
+    //SETTERS
+
+    public void setPrice(double price) {
         this.price = price;
     }
 
-    public Book(String name, Author author, double price, int qty) {
-        this.name = name;
-        this.author = author;
-        this.price = price;
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
         this.qty = qty;
     }
 
-    public String getName() {return name;}
+    //MÉTODOS
 
-    public Author getAuthor() {return author;}
-
-    public double getPrice() {return price;}
-
-    public void setPrice(double price) {this.price = price;}
-
-    public int getQty() {return qty;}
-
-    public void setQty(int qty) {this.qty = qty;}
-
-    public String toString() {
-        return "Book[name=" + name + ", " + author.toString() +
-                ", price=" + price + ", qty=" + qty + "]";
+    public String toString(){
+        return "Book[name="+name+","+author+",price="+price+",qty="+qty+"]";
     }
-
-    public String getAuthorName() {return author.getName();}
-    public String getAuthorEmail() {return author.getEmail();}
-    public char getAuthorGender() {return author.getGender();}
 }
