b2b072a06d3112b2fae233751dc1063a5248563b
Isa <isabelgp.ins@gmail.com>
2026-03-18 22:57:21 +0100

    Revert "Add files via upload"

--- src/Practica1d/GeometricObject/GeometricObject.java
+++
@@ -1,8 +0,0 @@
-package Practica1d.GeometricObject;
-
-//Interfaz la cual devuelve área y perimetro del objeto que la implementa
-
-public interface GeometricObject {
-    double getArea();
-    double getPerimeter();
-}
