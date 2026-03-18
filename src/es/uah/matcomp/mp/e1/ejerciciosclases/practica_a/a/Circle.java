0fbe21f34f141672b2e7157c6cf43fca215d778a
Isa <isabelgp.ins@gmail.com>
2026-03-18 21:48:58 +0100

    Update Circle.java

--- src/es/uah/matcomp/mp/e1/ejerciciosclases/practica_a/a/Circle.java
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/practica_a/a/Circle.java
@@ -3,32 +3,39 @@ package es.uah.matcomp.mp.e1.ejerciciosclases.practica_a.a;
 public class Circle {
 
     //Atributos
-    private double radius = 1.0;
-
+    private double radius;
+    private String color;
 
     //Constructores
+    //Constructor por defecto de la clase
     public Circle() {
+        this.radius = 1.0;
+        this.color = "red";
     }
 
+    //Constructor usando el valor del radio dado y color por defecto
     public Circle(double radius) {
         this.radius = radius;
+        this.color = "red";
     }
 
-
     //Métodos
+    //Devuelve el radio
     public double getRadius() {
         return radius;
     }
-
+    
+    //Asigna un valor a la variable radio
     public void setRadius(double radius) {
         this.radius = radius;
     }
-
-
+    
+    //Cálculo del área del círculo
     public double getArea() {
-        return Math.PI * radius * radius;
+        return Math.PI * Math.pow(radius,2);
     }
-
+    
+    //Cálculo de la circunferencia del círculo
     public double getCircumference() {
         return 2 * Math.PI * radius;
     }
